8a53e74b89a479a4f5bd97ddf2187bacb7ebda53
Marcelo Carlos <marcelo.carlos@avantiplc.com>
2014-01-30 17:32:07 +0000

    updating scripts to support -f paramenter

--- copyfiles.sh
+++ copyfiles.sh
@@ -1,24 +1,63 @@
 #!/bin/bash
+# copyfile.sh
+# Copyright 2012 - Marcelo Carlos (contact@marcelocarlos.com)
+# Distributed under the terms of the GNU General Public License, v2 or later
 
 source functions
 
-function copyIt() {
-	echo -ne "Copying dotfiles ... "
-	for dotfile in $(/bin/ls -1 | grep -v files.sh | grep -v README.md); do
-		FOLDER=$(realpath $dotfile)
-		if [ -e "$HOME/.$dotfile" ] || [ -L "$HOME/.$dotfile" ]; then
-			rm "$HOME/.$dotfile"
-		fi
-		cp -f "$FOLDER/$dotfile" "$HOME/.$dotfile"
-	done
-	echo "[OK]"
+# GLOBAL VARIABLES
+APP_NAME=$(basename $0)
+APP_PATH=$(dirname "$0")
+
+FORCE_W=0
+
+function usage() {
+    cat << EOF
+Usage: $APP_NAME [options]
+
+Options:
+    -f          overwrite files without asking for confirmation
+    -h          show this menu
+
+EOF
 }
 
-read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-	copyIt
+# a colon means that a parameter is expected after the option selected
+while getopts "hf" OPTION
+do
+    case $OPTION in
+        f)
+            FORCE_W=1
+            ;;
+        ?)
+            usage
+            exit
+            ;;
+    esac
+done
+
+function copyIt() {
+    echo -ne "Copying dotfiles ... "
+    for dotfile in $(/bin/ls -1 | grep -v files.sh | grep -v README.md); do
+        FOLDER=$(realpath $dotfile)
+        if [ -e "$HOME/.$dotfile" ] || [ -L "$HOME/.$dotfile" ]; then
+            rm -f "$HOME/.$dotfile"
+        fi
+        cp -f "$FOLDER/$dotfile" "$HOME/.$dotfile"
+    done
+    echo "[OK]"
+}
+
+if [ $FORCE_W == 0 ]; then
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        copyIt
+    fi
+else
+    copyIt
 fi
+
 unset copyIt
 
 # apply the changes
